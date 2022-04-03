041770083448a26b8f14b4258abdbd9bfd431101
Maxwell <maxwellyin@icloud.com>
2022-04-03 10:02:54 -0400

    .

--- debug.sh
+++
@@ -1,5 +0,0 @@
-. start.sh
-
-fuser -k 5000/tcp
-export FLASK_ENV=development
-flask run
--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
-python3 server.py 8001 routes.json node 1 &
-python3 server.py 8002 routes.json node 2 &
-python3 server.py 8003 routes.json node 3 &
-python3 server.py 8004 routes.json node 4 &
-python3 server.py 8005 routes.json node 5 &
+. serverStart.sh
 
-sleep 2s
-
-python3 client.py
+fuser -k 5000/tcp
+export FLASK_ENV=development
+flask run
